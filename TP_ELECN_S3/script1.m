6ffe906717c39c558681260df7d06180318ace54
Astri_ <malo1.thiebaud@gmail.com>
2022-11-26 23:32:36 +0100

    add semilicons

--- TP_ELECN_S3/script1.m
+++ TP_ELECN_S3/script1.m
@@ -14,20 +14,20 @@ Im2 = Im*2;
 subplot(2,6,2);
 imshow(Im2);
 subplot(2,6,8);
-hist(Im2)
+hist(Im2);
 
 
 Im3 = Im2 < SeuilOptimal(Im2);
 
 subplot(2,6,3);
-imshow(Im3)
+imshow(Im3);
 
 
 SE = strel('disk',2);
 Im4 = imopen(imclose(Im3,SE),SE);
 
 subplot(2,6,4);
-imshow(Im4)
+imshow(Im4);
 
 
 A = regionprops(Im4,'Centroid','Area','PixelIdxList');
@@ -35,12 +35,12 @@ segm=segmentation(A);
 Im5 = ones(size(Im));
 Im5(segm) = 0;
 
-subplot(2,6,5)
-imshow(Im5)
+subplot(2,6,5);
+imshow(Im5);
 
 
 [x, y] = point_ref(Im5);
 Im6 = regiongrowing(Im,x,y,0.15);
 
-subplot(2,6,6)
-imshow(Im6)
+subplot(2,6,6);
+imshow(Im6);
